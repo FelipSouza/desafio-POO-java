c74c94ba90c53a52fb977b8dbb28184f457b2c01
FelipSouza <felipe.souza.facco@gmail.com>
2024-11-25 20:06:29 -0500

    Trabalhando encapsulamento.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do Curso Java");
+        curso1.setCargaHoraria(40);
 
-        //teste
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição do Curso JavaScript");
+        curso2.setCargaHoraria(30);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria JavaScript");
+        mentoria1.setDescricao("Descriçaõ da mentoria JavaScript");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
